4e475d103e877d7f794997f794dec71c029239da
MMIX2009 <btshilombo@comcast.net>
2025-04-11 03:18:09 +0000

    some editing

---
+++ hos1/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    <h1> This is module 01 of CS 628 at Cityu University</h1>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This is CS628
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
