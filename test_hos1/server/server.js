220b4ab7b08cd3df60bae7028854426f68c9462f
MMIX2009 <btshilombo@comcast.net>
2025-04-11 03:40:35 +0000

    server side code example

---
+++ test_hos1/server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+// console.log('this is CS628 class on full stack web development');
